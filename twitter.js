6e3e7738fbd10ac1a497d87ae2699d2123659bab
PloughedField <75412486+PloughedField@users.noreply.github.com>
2021-07-18 14:47:18 +0300

    Update twitter.js

--- twitter.js
+++ twitter.js
@@ -16,10 +16,10 @@ module.exports =
 }
 // Twitter Verification Keys
 const AuthenticationKeys = {
-  consumer_key: 'GOG1FOPMnejJGuwBnxKdaxIy9',
-  consumer_secret: 'rn1nCBfhbA6ESeMBqmdmU0LcktDG0WdSYMGRFueCIOwiNhMRn1',
-  token: '1066915601992560640-oOqHW7BU5piqpdeYJrwZbgRQGLRTRS',
-  token_secret: 'tsA7R0cVAkzq94TpCUbgnLRaljSS61YQIPS5cKAdRRf1z'
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  token: process.env.TOKEN,
+  token_secret: process.env.TOKEN_SECRET
 };
 // A function that searches for Twitter users (1)
 async function users_search(user_name) {
